fe0dae61023524dcfe072dab37b07d08bf926a24
maggierphunt <50377783+maggierphunt@users.noreply.github.com>
2019-06-23 22:54:39 +0100

    Update undergrounds-form-js.js
    updated JSS

--- undergrounds-form-js.js
+++ undergrounds-form-js.js
@@ -1,6 +1,38 @@
+$(function() {
 $("#submitButton").click(function(event)        {
 var text1=$("#CoffeeShopName").val();
 var text2=$("#coffeeURL").val();
 var text3=$("#coffeeAddress").val();
-$("#list").append('<li><a href=\"'+ text2+'\" target=\"_blank\">'+text1+'</a>'+', '+text3+'</li>');
+$("#CoffeeList").append('<li><a href=\"'+ text2+'\" target=\"_blank\">'+text1+'</a>'+', '+text3+'</li>');
 });
+});
+
+$(function() {
+      $("form[name='CoffeeShopAdd']").validate({
+         rules: {
+
+           CoffeeShopName: {
+             required: true,
+
+           }
+
+           coffeeAddress: {
+             required: true,
+           }
+
+           coffeeURL: {
+             required: true,
+
+         }
+       },
+         messages: {
+         coffeeShopName: "Please enter the coffee shop name",
+         coffeeAddress: "Please enter an address",
+         coffeeURL: "Please enter a URL",
+
+       },
+       submitHandler: function(form) {
+         form.submit();
+       }
+     });
+   });
