0b04777fc5436de7484cd0241c610ab9ca80288d
docela <50206536+docela@users.noreply.github.com>
2019-06-24 18:14:57 +0100

    Add files via upload
    Added tooltips stating the coffee shop names.
    Cut down the stops from 15 to 7.
    Added a js file for the hovering.

---
+++ mapstyle.js
@@ -0,0 +1,27 @@
+$('.one').hover(function() {
+    $(".tip_1").toggleClass('show');
+})
+
+$('.two').hover(function() {
+    $(".tip_2").toggleClass('show');
+})
+
+$('.three').hover(function() {
+    $(".tip_3").toggleClass('show');
+})
+
+$('.four').hover(function() {
+    $(".tip_4").toggleClass('show');
+})
+
+$('.five').hover(function() {
+    $(".tip_5").toggleClass('show');
+})
+
+$('.six').hover(function() {
+    $(".tip_6").toggleClass('show');
+})
+
+$('.seven').hover(function() {
+    $(".tip_7").toggleClass('show');
+})
